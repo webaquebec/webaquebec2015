3772ea62da79e09654aa493b8f5094fdf6cef8ba
Maxime Potvin <mpotvin@o2web.ca>
2014-12-24 11:41:13 -0500

    footer, social missing

--- web/app/themes/waq2015/footer.php
+++ web/app/themes/waq2015/footer.php
@@ -1,7 +1,67 @@
 			</main> <!-- /.container -->
-			<footer role="contentinfo" class="container">
-				<div class="content">
-					<h2>Espace dalhousie : le terminal</h2>
+			
+			<footer role="contentinfo" class="dark">
+				<div class="container">				
+					
+					<div class="clearfix">
+						<div class="content">
+							<h2 class="sub title">Espace dalhousie : le terminal</h2>
+							<p>
+								84 Dalhousie (Bil) <br>
+								Québec (Québec), G1K 4C4
+							</p>
+							<div class="border-bottom expandable"></div>
+						</div>
+					</div>
+					
+					<div class="cols info-general">
+						<div class="col wide">
+							<div class="content">
+								<h3 class="thin title">Informations générales</h3>
+								<p>
+									<a href="tel:1 877 334-2547">1 877 334-2547</a><br>
+									<a href="mailto:info@webaquebec.org">info@webaquebec.org</a>
+								</p>
+								<p>
+									<h3 class="thin title">Informations médias</h3>
+									<a href="tel:1 418 558-7455">1 418 558-7455</a><br>
+									<a href="mailto:medias@webaquebec.org">medias@webaquebec.org</a>
+								</p>
+							</div>
+						</div>
+						<div class="col narrow">
+							<div class="content">
+								<h4 class="lower title">Siège social</h4>
+								<p>
+									305 Charest Est, bureau 901,<br>
+									Québec, (Québec) G1K 3H3<br>
+									<a href="tel:1 418 647-3877">1 418 647-3877</a>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="cols sub-footer">
+						<div class="content">
+							<div class="col wide">
+								<span class="accessibility">
+									<a href="<?= get_site_url(); ?>/accessibilite" >Accessibilité</a>
+								</span>
+								<span class="contribute">
+									<h4 class="lower title">Contribuez au site </h4><a href="https://github.com/webaquebec/webaquebec2015" target="_blank" title="WAQ 2015 sur Github">WAQ2015</a>
+								</span>
+								<span class="archives">
+									<h4 class="lower title">Archives</h4><a href="http://2014.webaquebec.org/" target="_blank" title="Site web - Archive WAQ 2014">WAQ 2014</a><span class="separator">&#183;</span><a href="http://2013.webaquebec.org/" target="_blank" title="Site web - Archive WAQ 2013">WAQ 2013</a><span class="separator">&#183;</span><a href="http://2012.webaquebec.org/" target="_blank" title="Site web - Archive WAQ 2012">WAQ 2012</a>
+								</span>
+							</div>
+							<div class="col narrow">
+								<span class="conception">
+									<p>Conception et réalisation <div class="logo-o2"><img src="/img/@2x/logo-o2web.png" alt="o2Web" /></div></p>
+								</span>
+							</div>
+						</div>
+					</div>
+				
 				</div>
 			</footer>
 		
