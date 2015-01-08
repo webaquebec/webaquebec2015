f8a271cb44d9bff662f169ac8bd41279d217c60b
Charles Thibault <cthibault@o2web.ca>
2015-01-08 15:49:44 -0500

    scroll to

--- web/app/themes/waq2015/assets/js/main.js
+++ web/app/themes/waq2015/assets/js/main.js
@@ -71,6 +71,83 @@ jQuery(document).ready(function($){
     });
   }
 
+
+  //
+  //
+  // SCROLLTO
+  function scrollTo(arg){
+    var type = typeof(arg);
+
+    if(type=='object'){
+      var $trigger = $(this);
+      var $target = $( '#' + $trigger.parent().attr('class').split(" ")[0] );
+      if(!$target.length) return;
+      arg.preventDefault();
+      arg.stopPropagation();
+    }
+    else if(type=='string'){
+      var $target = $( '#' + arg );
+      if(!$target.length) return;
+    }
+    
+    waq.$doc.stop().animate({
+        scrollTop: $target.offset().top - (waq.loggedin ? 142 : 110)
+    }, 800, $.bez([0.5, 0, 0.225, 1]));
+    
+  }
+
+  waq.$menu.$links.on('click', scrollTo);
+
+
+  //
+  //
+  // ACTIVATE MENU ITEMS ON SCROLL
+  function activateMenuOnScroll(){
+    waq.$menu.$anchoredSections = $();
+    for(i=0;i<waq.$menu.$links.length; i++){
+        var $trigger = waq.$menu.$links.eq(i);
+        var $section = $( '#' + $trigger.parent().attr('class').split(" ")[0] );
+        if($section.length) waq.$menu.$anchoredSections.push($section[0]);
+    }
+    if(waq.$menu.$anchoredSections.length){
+        waq.$menu.$anchoredSections.scrollEvents({
+            flag: 'anchor',
+            offset: 150,
+            topUp: function(e){
+               var $target = waq.$menu.$links.parent().filter('.'+e.selection.attr('id'));
+               if($target.length){
+                  waq.$menu.$links.parent().removeClass('active');
+                  $target.addClass('active');
+               };
+            },
+            topDown: function(e){
+              var $target = waq.$menu.$links.parent().filter('.'+$(se.items[minMax(e.i,0,100)]).attr('id'));
+              if($target.length){
+                waq.$menu.$links.parent().removeClass('active');
+                $target.addClass('active');
+              }
+            }
+
+        });
+    }
+  }
+
+  activateMenuOnScroll();
+
+  //
+  //
+  // SCROLL TO HASHBANG
+  var hash = window.location.hash;
+  if(hash.indexOf('!')!=-1){
+    var klass = hash.replace(/#|!|\//g,'');
+    $win.on('load',function(){
+      scrollTo(klass);
+    });
+  }
+
+ 
+
+
   //
   //
   // ANIMATE EXPANDABLES
@@ -178,13 +255,5 @@ jQuery(document).ready(function($){
   }
 
 
-  //
-  //
-  //HASHBANG
-  var hash = window.location.hash;
-  if(hash.indexOf('!')!=-1){
-    var klass = hash.replace(/#|!|\//g,'');
-  
-  }
   
 });
