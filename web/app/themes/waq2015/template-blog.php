54764a8b1a5c3f53ec644f19512d6bcac5045964
Mathieu Pesant <pesantmathieu@gmail.com>
2015-01-08 14:26:57 -0500

    Add the small streampost and twitter style

--- web/app/themes/waq2015/template-blog.php
+++ web/app/themes/waq2015/template-blog.php
@@ -37,7 +37,7 @@ get_header_once();
           <div class="content">
               <div class="article-info">
                 <span class="meta small"><?php echo get_the_author(); ?> <span class="separator">&#183;</span> <?php echo get_the_date(); ?></span>
-                <h2 class="sub title">
+                <h1 class="sub title">
                   <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
                 </h2>
               </div>
@@ -49,8 +49,67 @@ get_header_once();
           <a href="<?= get_site_url(); ?>/actualites" class=""><span>Voir tous les articles</span></a>
         </div>
       </div>
-      <div class="col wide stream-container">
-
+      <div class="col wide stream">
+        <div class="cols-2">
+          <div class="left col">
+            <article class="stream-post border-bottom twitter">
+              <div class="post-infos">
+                <div class="thumb">
+                  <img src="http://2014.webaquebec.org/wp-content/uploads/2013/12/martin_huard.png" alt="USERNAME">
+                </div>
+                <div class="user">
+                  <span class="name">Web à Québec</span>
+                  <a href="#_" class="username">@webaquebec</a>
+                  <span class="separator">&#183;</span>
+                  <span class="date">6 Nov</span>
+                </div>
+              </div>
+              <div class="content">
+                <p>Petite danse de la victoire! Merci aux 150 personnes qui ont déjà confirmé leur présence pour le <a href="#_">#WAQ15</a></p>
+              </div>
+              <ul class="share">
+                <li>
+                  <a href="#_" class="reply">Répondre</a>
+                </li>
+                <li>
+                  <a href="#_" class="retweet">Retweeter</a>
+                </li>
+                <li>
+                  <a href="#_" class="favorite">Ajouter aux favoris</a>
+                </li>
+              </ul>
+            </article>
+          </div>
+          <div class="right col">
+            <article class="stream-post border-bottom twitter">
+              <div class="post-infos">
+                <div class="thumb">
+                  <img src="http://2014.webaquebec.org/wp-content/uploads/2013/12/martin_huard.png" alt="USERNAME">
+                </div>
+                <div class="user">
+                  <span class="name">Web à Québec</span>
+                  <a href="#_" class="username">@webaquebec</a>
+                  <span class="separator">&#183;</span>
+                  <span class="date">6 Nov</span>
+                </div>
+              </div>
+              <div class="content">
+                <p>Petite danse de la victoire! Merci aux 150 personnes qui ont déjà confirmé leur présence pour le <a href="#_">#WAQ15</a></p>
+              </div>
+              <ul class="share">
+                <li>
+                  <a href="#_" class="reply">Répondre</a>
+                </li>
+                <li>
+                  <a href="#_" class="retweet">Retweeter</a>
+                </li>
+                <li>
+                  <a href="#_" class="favorite">Ajouter aux favoris</a>
+                </li>
+              </ul>
+            </article>
+          </div>
+        </div>
       </div>
     </div>
   </div>
