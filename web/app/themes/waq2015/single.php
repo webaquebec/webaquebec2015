6f82bbbbfc3366d9bd8a53d35a167975d4c16d27
Mathieu Pesant <pesantmathieu@gmail.com>
2015-01-16 17:40:59 -0500

    Fix single.php responsive and title

--- web/app/themes/waq2015/single.php
+++ web/app/themes/waq2015/single.php
@@ -2,9 +2,9 @@
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
-<article class="single no-padding">
+<article class="article single no-padding">
 
-  <hgroup class="dark">
+<!--   <hgroup class="dark">
     <div class="container">
       <div class="main title border-left">
         <div class="article-info">
@@ -14,13 +14,24 @@
         <div class="border-bottom expandable"></div>
       </div>
     </div>
+  </hgroup> -->
+
+  <hgroup>
+    <div class="container">
+      <div class="main title">
+        <div class="article-info">
+          <h2 class="title"><?= get_the_author() ?> <span class="separator">&#183;</span> <?= get_the_date() ?></h2>
+        </div>
+        <h1><?= get_the_title() ?></h1>
+      </div>
+    </div>
   </hgroup>
 
   <div class="cols container">
     <section class="col wide" role="main">
       <div class="single-article">
         <div class="content">
-          <div class="image-une">
+          <div class="article-image">
             <?php
               if ( has_post_thumbnail() ) {
                   the_post_thumbnail('large');
