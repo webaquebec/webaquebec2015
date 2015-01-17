f019d45da2f5b027e3a7c0625dd74c556d681746
Mathieu Pesant <pesantmathieu@gmail.com>
2015-01-16 19:45:23 -0500

    Add a link on the partners' logos

--- web/app/themes/waq2015/template-partenaires.php
+++ web/app/themes/waq2015/template-partenaires.php
@@ -32,7 +32,13 @@ get_header_once();
       <ul class="logos grid">
       <?php foreach ($presenters as $presenter) : ?>
         <li>
-          <img src="<?= $presenter['image']['url']; ?>" alt="<?= $presenter['name']; ?>">
+          <?php if($presenter['url']) : ?>
+          <a href="<?= $presenter['url'] ?>" target="_blank">
+          <?php endif; ?>
+            <img src="<?= $presenter['image']['url']; ?>" alt="<?= $presenter['name']; ?>">
+          <?php if($presenter['url']) : ?>
+          </a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
       </ul>
@@ -44,7 +50,13 @@ get_header_once();
       <ul class="logos grid">
       <?php foreach ($main_partners as $main_partner) : ?>
         <li>
-          <img src="<?= $main_partner['image']['url']; ?>" alt="<?= $main_partner['name']; ?>">
+          <?php if($main_partner['url']) : ?>
+          <a href="<?= $main_partner['url'] ?>" target="_blank">
+          <?php endif; ?>
+            <img src="<?= $main_partner['image']['url']; ?>" alt="<?= $main_partner['name']; ?>">
+          <?php if($main_partner['url']) : ?>
+          </a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
       </ul>
@@ -56,7 +68,13 @@ get_header_once();
       <ul class="logos small grid">
       <?php foreach ($associates as $associate) : ?>
         <li>
-          <img src="<?= $associate['image']['url']; ?>" alt="<?= $associate['name']; ?>">
+          <?php if($associate['url']) : ?>
+          <a href="<?= $associate['url'] ?>" target="_blank">
+          <?php endif; ?>
+            <img src="<?= $associate['image']['url']; ?>" alt="<?= $associate['name']; ?>">
+          <?php if($associate['url']) : ?>
+          </a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
       </ul>
@@ -68,7 +86,13 @@ get_header_once();
       <ul class="logos small grid">
       <?php foreach ($publics as $public) : ?>
         <li>
-          <img src="<?= $public['image']['url']; ?>" alt="<?= $public['name']; ?>">
+          <?php if($public['url']) : ?>
+          <a href="<?= $public['url'] ?>" target="_blank">
+          <?php endif; ?>
+            <img src="<?= $public['image']['url']; ?>" alt="<?= $public['name']; ?>">
+          <?php if($public['url']) : ?>
+          </a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
       </ul>
@@ -80,7 +104,13 @@ get_header_once();
       <ul class="logos small grid">
       <?php foreach ($supporters as $supporter) : ?>
         <li>
-          <img src="<?= $supporter['image']['url']; ?>" alt="<?= $supporter['name']; ?>">
+          <?php if($supporter['url']) : ?>
+          <a href="<?= $supporter['url'] ?>" target="_blank">
+          <?php endif; ?>
+            <img src="<?= $supporter['image']['url']; ?>" alt="<?= $supporter['name']; ?>">
+          <?php if($supporter['url']) : ?>
+          </a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
       </ul>
@@ -92,7 +122,13 @@ get_header_once();
       <ul class="logos small grid">
       <?php foreach ($medias as $media) : ?>
         <li>
-          <img src="<?= $media['image']['url']; ?>" alt="<?= $media['name']; ?>">
+          <?php if($media['url']) : ?>
+          <a href="<?= $media['url'] ?>" target="_blank">
+          <?php endif; ?>
+            <img src="<?= $media['image']['url']; ?>" alt="<?= $media['name']; ?>">
+          <?php if($media['url']) : ?>
+          </a>
+          <?php endif; ?>
         </li>
       <?php endforeach; ?>
       </ul>
