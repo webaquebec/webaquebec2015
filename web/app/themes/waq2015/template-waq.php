8991456e4b29f44012447dde9f6b6f0825c545f1
Charles Thibault <cthibault@o2web.ca>
2015-01-18 15:33:46 -0500

    fixed members section : hide sections with no members, show only social links with values, removed hover when no social link at all

--- web/app/themes/waq2015/template-waq.php
+++ web/app/themes/waq2015/template-waq.php
@@ -40,312 +40,130 @@ get_header_once();
       </div>
     </div>
     <div class="bottom">
+      <?php
+
+      function has_social($member){
+        return has($member['facebook']) || has($member['facebook']) || has($member['facebook']);
+      }
+
+      function social_links($member){ ?>
+        <nav class="links">
+          <ul>
+          <?php if(has($member['facebook'])): ?>
+            <li><a href="<?= $member['facebook'] ?>" target="_blank" class="facebook">Facebook</a></li>
+          <?php endif;
+          if(has($member['twitter'])): ?>
+            <li><a href="<?= $member['twitter'] ?>" target="_blank" class="twitter">Twitter</a></li>
+          <?php endif;
+          if(has($member['linked_in'])): ?>
+            <li><a href="<?= $member['linked_in'] ?>" target="_blank" class="linkedin">Linkedin</a></li>
+          <?php endif; ?>
+          </ul>
+        </nav>
+      <?php
+      }
+
+      function list_members($title, $members){
+        if(has($members)):
+        ?>
+        <h2 class="sub title lined"><?= $title ?></h2>
+        <hr class="sub-line">
+        <ul class="<?= sanitize_title($title) ?>'-list grid">
+        <?php foreach ($members as $member) : ?>
+         <li>
+            <figure class="profile<?php if(has_social($member)) echo ' has-social'; ?>">
+              <div>
+                <div class="thumb">
+                  <img src="<?= $member['image']['sizes']['thumbnail']; ?>" alt="<?= $member['name']; ?>" />
+                </div>
+
+                <figcaption class="infos">
+                  <h3 class="name"><?= $member['name']; ?></h3>
+                    <?php social_links($member); ?>
+                </figcaption>
+              </div>
+            </figure>
+          </li>
+        <?php endforeach; ?>
+        </ul>
+        <?php
+        endif;
+      }
+
+      // List Members
+      ?>
       <div class="board group">
         <?php
-
         // board of directors
         $board_members = get_field('board');
-
+        list_members(__('Conseil d\'administration','waq'), $board_members);
         ?>
-        <h2 class="sub title lined">Conseil d'administration</h2>
-        <hr class="sub-line">
-        <ul class="board-members-list grid">
-        <?php foreach ($board_members as $board_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $board_member['image']['sizes']['thumbnail']; ?>" alt="<?= $board_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $board_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $board_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $board_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $board_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="welcome group">
         <?php
-
         // welcome
         $welcome_members = get_field('welcome');
-
+        list_members(__('Accueil','waq'), $welcome_members);
         ?>
-        <h2 class="sub title lined">Accueil</h2>
-        <hr class="sub-line">
-        <ul class="welcome-members-list grid">
-        <?php foreach ($welcome_members as $welcome_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $welcome_member['image']['sizes']['thumbnail']; ?>" alt="<?= $welcome_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $welcome_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $welcome_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $welcome_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $welcome_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="admin group">
         <?php
-
         // admin
         $admin_members = get_field('admin');
-
+        list_members(__('Administration','waq'), $admin_members);
         ?>
-        <h2 class="sub title lined">Administration</h2>
-        <hr class="sub-line">
-        <ul class="admin-members-list grid">
-        <?php foreach ($admin_members as $admin_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $admin_member['image']['sizes']['thumbnail']; ?>" alt="<?= $admin_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $admin_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $admin_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $admin_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $admin_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="volounteers-committee group">
         <?php
-
         // volounteers
         $volounteers_committee_members = get_field('volounteers_committee');
-
+        list_members(__('Bénévoles','waq'), $volounteers_committee_members);
         ?>
-        <h2 class="sub title lined">Bénévoles</h2>
-        <hr class="sub-line">
-        <ul class="volounteers-committee-members-list grid">
-        <?php foreach ($volounteers_committee_members as $volounteers_committee_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $volounteers_committee_member['image']['sizes']['thumbnail']; ?>" alt="<?= $volounteers_committee_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $volounteers_committee_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $volounteers_committee_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $volounteers_committee_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $volounteers_committee_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="communications group">
         <?php
-
         // communications
         $communications_members = get_field('communications');
-
+        list_members(__('Communications','waq'), $communications_members);
         ?>
-        <h2 class="sub title lined">Communications</h2>
-        <hr class="sub-line">
-        <ul class="communications-members-list grid">
-        <?php foreach ($communications_members as $communications_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $communications_member['image']['sizes']['thumbnail']; ?>" alt="<?= $communications_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $communications_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $communications_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $communications_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $communications_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="logisitics group">
         <?php
-
         // logistics
         $logistics_members = get_field('logistics');
-
+        list_members(__('Logisitique','waq'), $logistics_members);
         ?>
-        <h2 class="sub title lined">Logisitique</h2>
-        <hr class="sub-line">
-        <ul class="logistics-members-list grid">
-        <?php foreach ($logistics_members as $logistics_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $logistics_member['image']['sizes']['thumbnail']; ?>" alt="<?= $logistics_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $logistics_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $logistics_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $logistics_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $logistics_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="schedule group">
         <?php
-
         // schedule
         $schedule_members = get_field('schedule');
-
+        list_members(__('Programmation','waq'), $schedule_members);
         ?>
-        <h2 class="sub title lined">Programmation</h2>
-        <hr class="sub-line">
-        <ul class="schedule-members-list grid">
-        <?php foreach ($schedule_members as $schedule_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $schedule_member['image']['sizes']['thumbnail']; ?>" alt="<?= $schedule_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $schedule_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $schedule_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $schedule_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $schedule_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="welcome group">
         <?php
-
         // protocol
         $protocol_members = get_field('protocol');
-
+        list_members(__('Protocole','waq'), $protocol_members);
         ?>
-        <h2 class="sub title lined">Protocole</h2>
-        <hr class="sub-line">
-        <ul class="wprotocol-list grid">
-        <?php foreach ($protocol_members as $protocol_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $protocol_member['image']['sizes']['thumbnail']; ?>" alt="<?= $protocol_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $protocol_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $protocol_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $protocol_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $protocol_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
       <div class="international group">
         <?php
-
         // international
         $international_members = get_field('international');
-
+        list_members(__('Relations internationales','waq'), $international_members);
         ?>
-        <h2 class="sub title lined">Relations internationales</h2>
-        <hr class="sub-line">
-        <ul class="international-members-list grid">
-        <?php foreach ($international_members as $international_member) : ?>
-          <li>
-            <figure class="profile">
-              <div>
-                <div class="thumb">
-                  <img src="<?= $international_member['image']['sizes']['thumbnail']; ?>" alt="<?= $international_member['name']; ?>" />
-                </div>
-
-                <figcaption class="infos">
-                  <h3 class="name"><?= $international_member['name']; ?></h3>
-                  <nav class="links">
-                    <ul>
-                      <li><a href="<?= $international_member['facebook']; ?>" target="_blank" class="facebook">facebook</a></li>
-                      <li><a href="<?= $international_member['twitter']; ?>" target="_blank" class="twitter">twitter</a></li>
-                      <li><a href="<?= $international_member['linked_in']; ?>" target="_blank" class="linkedin">Linkedin</a></li>
-                    </ul>
-                  </nav>
-                </figcaption>
-              </div>
-            </figure>
-          </li>
-        <?php endforeach; ?>
-        </ul>
       </div>
+
     </div>
   </div>
 
