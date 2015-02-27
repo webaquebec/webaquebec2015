e521c84e470364ecf5b0f1336e434b342a3c4de8
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2015-02-27 17:50:40 -0500

    Add page content

--- web/app/themes/waq2015/page.php
+++ web/app/themes/waq2015/page.php
@@ -9,8 +9,19 @@
       <div class="border-bottom"></div>
     </h1>
   </header>
-
 </section>
+
+<div class="cols container">
+  <section class="col wide" role="main">
+    <div class="post">
+      <div class="content">
+        <?php the_content() ?>
+      </div>
+
+    </div>
+
+  </section>
+</div>
 
 <?php endwhile; endif; ?>
 
