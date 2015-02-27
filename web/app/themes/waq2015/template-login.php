91ee301f0d13007ea0f18cb69b04e0d2d625e404
Charles Thibault <cthibault@o2web.ca>
2015-02-27 09:06:18 -0500

    fix redirect loops and errors on login page

--- web/app/themes/waq2015/template-login.php
+++ web/app/themes/waq2015/template-login.php
@@ -32,8 +32,8 @@ if(have_posts()): while(have_posts()): the_post();
 
           <div class="tab-contents">
             <?php
-            $login =  isset($_GET['login']) ? explode(' ', $_GET['login']) : [];
-            $registration =  isset($_GET['registration']) ? explode(' ', $_GET['registration']) : [];
+            $login_errors =  isset($_GET['login']) ? explode(' ', $_GET['login']) : [];
+            $registration_errors =  isset($_GET['registration']) ? explode(' ', $_GET['registration']) : [];
             $success =  isset($_GET['success']);
             ?>
 
@@ -49,14 +49,7 @@ if(have_posts()): while(have_posts()): the_post();
               <div class="social-account">
                 <h3>
                   <div class="message"><?= __('Connectez-vous avec') ?></div>
-                  <?php
-                    ob_start();
-                    do_action( 'wordpress_social_login' );
-                    $loginForm = ob_get_contents();
-                    ob_end_clean();
-                    $loginForm = preg_replace('/<a(.*?)>(.*?)<\/a>/is', "<a$1 tab-index=\"2\"><span>$2</span></a>", $loginForm);
-                    echo $loginForm;
-                  ?>
+                  <?= fb_login_form() ?>
                 </h3>
               </div>
 
@@ -65,20 +58,7 @@ if(have_posts()): while(have_posts()): the_post();
               <?php endif; ?>
 
               <div class="wp-account">
-                <?php
-                $loginForm = wp_login_form( array(
-                  'echo' => false,
-                  'id_submit' => 'submit-login',
-                  'redirect'       => get_permalink(get_ID_from_slug('mon-horaire')),
-                  'label_username' => __( 'Nom d\'utilisateur', 'waq' ),
-                      'label_password' => __( 'Mot de passe', 'waq' ),
-                      'label_remember' => __( 'Rester connecté', 'waq' ),
-                      'label_log_in'   => __( 'Connexion','waq' ),
-                      'value_username' =>isset($_GET['user']) ? urldecode($_GET['user']) : NULL
-                ));
-                $loginForm = preg_replace('/<p(.*?)>(.*?)<\/p>/is', "<div class=\"field\"><p$1>$2</p></div>", $loginForm);
-                echo $loginForm;
-                ?>
+                <?= user_login_form($login_errors) ?>
               </div>
 
             </div>
@@ -96,14 +76,7 @@ if(have_posts()): while(have_posts()): the_post();
               <div class="social-account">
                 <h3>
                   <div class="message"><?= __('Connectez-vous avec') ?></div>
-                  <?php
-                    ob_start();
-                    do_action( 'wordpress_social_login' );
-                    $loginForm = ob_get_contents();
-                    ob_end_clean();
-                    $loginForm = preg_replace('/<a(.*?)>(.*?)<\/a>/is', "<a$1 tab-index=\"2\"><span>$2</span></a>", $loginForm);
-                    echo $loginForm;
-                  ?>
+                  <?= fb_login_form() ?>
                 </h3>
               </div>
 
@@ -116,11 +89,11 @@ if(have_posts()): while(have_posts()): the_post();
                 <div class="field required">
                   <label for="user_login"><?= __( 'Nom d\'utilisateur', 'waq' ) ?></label>
                   <?php // empty username
-                  if(in_array('empty_username', $registration)):?>
+                  if(in_array('empty_username', $registration_errors)):?>
                     <p class="error message note"><?= __( 'Un nom d\'utilisateur est requis', 'waq' ) ?></p>
                   <?php endif; ?>
                   <?php // username already exists
-                  if(in_array('username_exists', $registration)):?>
+                  if(in_array('username_exists', $registration_errors)):?>
                     <p class="error message note"><?= __( 'Le nom d\'utilisateur est déjà utilisé', 'waq' ) ?></p>
                   <?php endif; ?>
                   <input type="text" name="user_login" id="user_login" class="input" <?php if(isset($_GET['user'])) echo 'value="'.urldecode($_GET['user']).'"'; ?>/>
@@ -134,15 +107,15 @@ if(have_posts()): while(have_posts()): the_post();
                 <div class="field required">
                   <label for="user_email"><?= __( 'Adresse courriel', 'waq' ) ?></label>
                   <?php // email is empty
-                  if(in_array('empty_email', $registration)):?>
+                  if(in_array('empty_email', $registration_errors)):?>
                     <p class="error message note"><?= __( 'Une adresse courriel est requise', 'waq' ) ?></p>
                   <?php endif; ?>
                   <?php // email is invalid
-                  if(in_array('invalid_email', $registration)):?>
+                  if(in_array('invalid_email', $registration_errors)):?>
                     <p class="error message note"><?= __('Veuillez entrer une adresse courriel valide','waq') ?></p>
                   <?php endif; ?>
                   <?php // email already exists
-                  if(in_array('email_exists', $registration)):?>
+                  if(in_array('email_exists', $registration_errors)):?>
                     <p class="error message note"><?= __( 'L\'adresse courriel est déjà utilisée', 'waq' ) ?></p>
                   <?php endif; ?>
                   <input type="text" name="user_email" id="user_email" class="input" <?php if(isset($_GET['email'])) echo 'value="'.urldecode($_GET['email']).'"'; ?> />
@@ -151,7 +124,7 @@ if(have_posts()): while(have_posts()): the_post();
                 <div class="field required">
                   <label for="user_password"><?= __( 'Mot de passe', 'waq' ) ?></label>
                   <?php // passowrd is empty
-                  if(in_array('password_missing', $registration)):?>
+                  if(in_array('password_missing', $registration_errors)):?>
                     <p class="error message note"><?= __( 'Un mot de passe est requis', 'waq' ) ?></p>
                   <?php endif; ?>
                   <input type="password" name="user_password" id="user_password" class="input" />
@@ -160,7 +133,7 @@ if(have_posts()): while(have_posts()): the_post();
                 <div class="field required">
                   <label for="user_password_repeat"><?= __( 'Répétez le mot de passe', 'waq' ) ?></label>
                   <?php // passowrd_repeat is empty
-                  if(in_array('password_repeat_missing', $registration)):?>
+                  if(in_array('password_repeat_missing', $registration_errors)):?>
                     <p class="error message note"><?= __( 'Rétépez le mot de passe ici', 'waq' ) ?></p>
                   <?php endif; ?>
                   <input type="password" name="user_password_repeat" id="user_password_repeat" class="input"/>
