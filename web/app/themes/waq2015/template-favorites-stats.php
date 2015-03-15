ae2164418c7a97bc62ae213adcbd25a88c35a93e
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2015-03-15 17:51:48 -0400

    Add content to stats & fix older php function limit

--- web/app/themes/waq2015/template-favorites-stats.php
+++ web/app/themes/waq2015/template-favorites-stats.php
@@ -25,43 +25,47 @@
           <span class="sub title">Les favoris populaires</span>
         </span>
       </h2>
-      <?php
-      // Select users favorites
-      $favorites = $wpdb->get_col("SELECT meta_value FROM  `wp_usermeta` WHERE `meta_key` =  'favorites';");
-      $favorites_sums = array();
+      <div class="content">
+        <?php the_content() ?>
+        <?php
+        // Select users favorites
+        $favorites = $wpdb->get_col("SELECT meta_value FROM  `wp_usermeta` WHERE `meta_key` =  'favorites';");
+        $favorites_sums = array();
 
-      // Sum favorites per post ID
-      foreach ($favorites as $values) {
-        if(empty(trim($values))) continue;
-        $ids = explode('|', $values);
-        foreach ($ids as $id) {
-          if(empty($favorites_sums[$id])) {
-            $favorites_sums[$id] = 1;
-          } else {
-            $favorites_sums[$id] += 1;
+        // Sum favorites per post ID
+        foreach ($favorites as $values) {
+          $values = trim($values);
+          if(empty($values)) continue;
+          $ids = explode('|', $values);
+          foreach ($ids as $id) {
+            if(empty($favorites_sums[$id])) {
+              $favorites_sums[$id] = 1;
+            } else {
+              $favorites_sums[$id] += 1;
+            }
           }
         }
-      }
 
-      // Sort by popularity
-      arsort($favorites_sums);
-      $posts_ids = array_keys($favorites_sums);
-      $posts_ids = implode(',', $posts_ids);
+        // Sort by popularity
+        arsort($favorites_sums);
+        $posts_ids = array_keys($favorites_sums);
+        $posts_ids = implode(',', $posts_ids);
 
-      // Fetch post details we need
-      $posts = $wpdb->get_results("SELECT `id`, `post_title`, `post_name` FROM  `wp_posts` WHERE id IN($posts_ids)");
-      $posts_details = array();
-      foreach ($posts as $post) {
-        $posts_details[$post->id] = $post;
-      }
-      ?>
-      <ul>
-        <?php foreach ($favorites_sums as $post_id => $sum): ?>
-          <?php if($sum < 5) continue; ?>
-          <?php $post = $posts_details[$post_id]; ?>
-          <li><?php echo $sum; ?> <a href="/activite/<?php echo $post->post_name; ?>"><?php echo $post->post_title; ?></a></li>
-        <?php endforeach ?>
-      </ul>
+        // Fetch post details we need
+        $posts = $wpdb->get_results("SELECT `id`, `post_title`, `post_name` FROM  `wp_posts` WHERE id IN($posts_ids)");
+        $posts_details = array();
+        foreach ($posts as $post) {
+          $posts_details[$post->id] = $post;
+        }
+        ?>
+        <ul>
+          <?php foreach ($favorites_sums as $post_id => $sum): ?>
+            <?php if($sum < 5) continue; ?>
+            <?php $post = $posts_details[$post_id]; ?>
+            <li><?php echo $sum; ?> <a href="/activite/<?php echo $post->post_name; ?>"><?php echo $post->post_title; ?></a></li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
 
     </div>
 
